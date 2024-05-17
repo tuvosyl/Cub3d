6f9ea8231afe4d814369595b4c78fc70a1e39193
Mael Gallais <mgallais@c2r12p2.42lehavre.fr>
2024-05-17 09:17:52 +0200

    collision

---
+++ src/raycaster/collisions.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   collisions.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 09:03:59 by mgallais          #+#    #+#             */
+/*   Updated: 2024/05/17 09:14:50 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+int	is_wall(t_data *data, t_2int pos)
+{
+	int	x;
+	int	y;
+
+	x = pos.x;
+	y = pos.y;
+	if (x < 0 || y < 0 || x >= data->map.map_size.x || y >= data->map.map_size.y)
+		return (1);
+	return (data->map.map[y][x] == '1');
+}
+
+// bonus
+int	is_sprite(t_data *data, t_2int pos)
+{
+	int	x;
+	int	y;
+
+	x = pos.x;
+	y = pos.y;
+	if (x < 0 || y < 0 || x >= data->map.map_size.x || y >= data->map.map_size.y)
+		return (0);
+	return (data->map.map[y][x] == '2');
+}

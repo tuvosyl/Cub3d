cd5b0647ba3904b12d0c9dcbad381e578be557d2
tuvosyl <112665968+tuvosyl@users.noreply.github.com>
2024-06-06 15:19:19 +0200

    insh

--- src/game/raycaster.c
+++ src/game/raycaster.c
@@ -3,25 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   raycaster.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vsoltys <vsoltys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 09:18:09 by mgallais          #+#    #+#             */
-/*   Updated: 2024/06/06 14:54:37 by mgallais         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:19:09 by vsoltys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
+void	convert_to_hex(uint32_t *texture_map, uint8_t *pixels)
+{
+	int	i;
+	t_4int color;
+
+	i = 0;
+	while (i < TEXTURE_SIZE * TEXTURE_SIZE)
+	{
+		color.r = pixels[i * 4];
+		color.g = pixels[i * 4 + 1];
+		color.b = pixels[i * 4 + 2];
+		color.l = pixels[i * 4 + 3];
+		texture_map[i] = ft_pixel(color.r, color.g, color.b, color.l);
+		i++;
+	}
+}
+// Fonction principale pour dessiner les rayons avec textures
+// void draw_rays(t_data *data, t_raywall *rays)
+// {
+//     int height;
+//     uint32_t *texture;
+//     uint32_t color;
+//     t_2int incr;
+
+//     incr.x = 0;
+//     texture = malloc(sizeof(uint32_t) * TEXTURE_SIZE * TEXTURE_SIZE);
+//     mlx_delete_image(data->mlx, data->camera_view);
+//     data->camera_view = mlx_new_image(data->mlx, data->screen_size.x, data->screen_size.y);
+//     convert_to_hex(texture, data->textures.north_texture->pixels);
+//     while (incr.x < data->screen_size.x)
+//     {
+//         height = (int)(data->screen_size.y - rays[incr.x].distance * 32);
+//         if (height < 0)
+//             height = 0;
+//         incr.y = data->screen_size.y / 2 - height / 2;
+
+//         // Calcul de la coordonnée de texture x
+//         //int texture_x = (int)(incr.x * TEXTURE_SIZE) % TEXTURE_SIZE;  // Supposons que la largeur de la texture soit de 64 pixels
+
+//         while (incr.y < data->screen_size.y / 2 + height / 2)
+//         {
+//             // Calcul de la coordonnée de texture y
+//            // float fog_factor = 1.0 - (rays[incr.x].distance / MAX_DISTANCE * 10);
+//             int texture_y = ((incr.y - (data->screen_size.y / 2 - height / 2)) * TEXTURE_SIZE) / height;  // Supposons que la hauteur de la texture soit de 64 pixels
+//             texture_y = fmin(fmax(texture_y, 0), 63); // S'assurer que les coordonnées sont dans les limites
+//             color = texture[TEXTURE_SIZE * texture_y + rays[incr.x].texture_pos % TEXTURE_SIZE]; // Obtenir la couleur de la texture
+//             mlx_put_pixel(data->camera_view, incr.x, incr.y, color);
+//             incr.y++;
+//         }
+//         incr.x++;
+//     }
+
+//     // Afficher l'image de la vue de la caméra à la fenêtre
+//     mlx_image_to_window(data->mlx, data->camera_view, 0, 0);
+// }
 void	draw_rays(t_data *data, t_raywall *rays)
 {
 	int		height;
-	int		color;
+	uint32_t *texture;
+    uint32_t color;
 	t_2int	incr;
 
+	texture = malloc(sizeof(uint32_t) * TEXTURE_SIZE * TEXTURE_SIZE);
 	incr.x = 0;
 	mlx_delete_image(data->mlx, data->camera_view);
 	data->camera_view = mlx_new_image(data->mlx,
 			data->screen_size.x, data->screen_size.y);
+	convert_to_hex(texture, data->textures.north_texture->pixels);
 	while (incr.x != data->screen_size.x)
 	{
 		height = (data->screen_size.y * 0.5f) / tan(FOV * 0.5f) / rays[incr.x].distance * 5;
@@ -32,7 +90,10 @@ void	draw_rays(t_data *data, t_raywall *rays)
 		incr.y = (data->screen_size.y / 2 - height / 2);
 		while (incr.y != data->screen_size.y / 2 + height / 2)
 		{
-			color = ft_pixel(109, 37, 190, 255);
+			// float fog_factor = 1.0 - (rays[incr.x].distance / MAX_DISTANCE * 10);
+			int texture_y = ((incr.y - (data->screen_size.y / 2 - height / 2)) * TEXTURE_SIZE) / height;  // Supposons que la hauteur de la texture soit de 64 pixels
+            texture_y = fmin(fmax(texture_y, 0), 63); 
+			color = texture[TEXTURE_SIZE * texture_y + (rays[incr.x].texture_pos % TEXTURE_SIZE)];
 			mlx_put_pixel(data->camera_view, incr.x, incr.y, color);
 			incr.y++;
 		}

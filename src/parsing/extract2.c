da9fc93cdd9321487a75eeff7c86c61e7ae7d45d
tuvosyl <112665968+tuvosyl@users.noreply.github.com>
2024-06-17 18:48:48 +0200

    insh

--- src/parsing/extract2.c
+++ src/parsing/extract2.c
@@ -6,7 +6,7 @@
 /*   By: vsoltys <vsoltys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 17:01:23 by vsoltys           #+#    #+#             */
-/*   Updated: 2024/06/17 17:23:04 by vsoltys          ###   ########.fr       */
+/*   Updated: 2024/06/17 18:01:49 by vsoltys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-
-// char	*extract_rgb_2(char *str, int *i, int *which_value)
-// {
-// 	int		counter;
-// 	int		counter_temp;
-// 	char	*temp;
-
-// 	counter = 0;
-// 	counter_temp = 0;
-// 	while (ft_isdigit(str[*i]) == 1 && str[*i] && str[*i] != ',')
-// 	{
-// 		(*i)++;
-// 		counter++;
-// 	}
-// 	temp = malloc(sizeof(char) * counter + 1);
-// 	*i -= counter;
-// 	while (counter_temp != counter)
-// 	{
-// 		temp[counter_temp] = str[*i + counter_temp];
-// 		counter_temp++;
-// 	}
-// 	temp[counter_temp] = '\0';
-// 	*i += counter;
-// 	(*which_value)++;
-// 	return (temp);
-// }
-
-void extract_value_2_2(t_data *data, char *str)
+void	extract_value_2_2(t_data *data, char *str)
 {
 	if (str[0] == 'E' && str[1] == 'A')
 	{

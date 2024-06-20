0126677f513148b722052d155b6b4961548d1e9d
tuvosyl <112665968+tuvosyl@users.noreply.github.com>
2024-06-20 11:04:07 +0200

    insh

--- src/parsing/load_texture.c
+++ src/parsing/load_texture.c
@@ -6,7 +6,7 @@
 /*   By: vsoltys <vsoltys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:32:40 by vsoltys           #+#    #+#             */
-/*   Updated: 2024/06/20 11:00:55 by vsoltys          ###   ########.fr       */
+/*   Updated: 2024/06/20 11:03:43 by vsoltys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,25 @@ void	error(t_data *data)
 	free_data(data);
 	ft_printf("Error\n↪\tcant open a file\n");
 	exit(EXIT_FAILURE);
+}
+
+void	load_png_3(t_data *data)
+{
+	data->textures.west_texture = mlx_load_png(data->texture_path.west_texture);
+	if (!data->textures.west_texture)
+	{
+		mlx_delete_texture(data->textures.north_texture);
+		mlx_delete_texture(data->textures.south_texture);
+		error(data);
+	}
+	data->textures.east_texture = mlx_load_png(data->texture_path.east_texture);
+	if (!data->textures.east_texture)
+	{
+		mlx_delete_texture(data->textures.north_texture);
+		mlx_delete_texture(data->textures.south_texture);
+		mlx_delete_texture(data->textures.west_texture);
+		error(data);
+	}
 }
 
 void	load_png_2(t_data *data)
@@ -38,21 +57,7 @@ void	load_png_2(t_data *data)
 		mlx_delete_texture(data->textures.north_texture);
 		error(data);
 	}
-	data->textures.west_texture = mlx_load_png(data->texture_path.west_texture);
-	if (!data->textures.west_texture)
-	{
-		mlx_delete_texture(data->textures.north_texture);
-		mlx_delete_texture(data->textures.south_texture);
-		error(data);
-	}
-	data->textures.east_texture = mlx_load_png(data->texture_path.east_texture);
-	if (!data->textures.east_texture)
-	{
-		mlx_delete_texture(data->textures.north_texture);
-		mlx_delete_texture(data->textures.south_texture);
-		mlx_delete_texture(data->textures.west_texture);
-		error(data);
-	}
+	load_png_3(data);
 }
 
 void	load_png(t_data *data)
